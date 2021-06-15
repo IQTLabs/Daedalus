84263078aeb1d2c3fb8ba38206bc8b5d3c217c98
Josh Bailey <josh@vandervecken.com>
2021-06-15 04:49:04 +0000

    wait for dovesnap to clean up before stopping dovesnap.

--- blue/5g/shutdown.sh
+++ blue/5g/shutdown.sh
@@ -4,5 +4,12 @@ DC_YAML=""
 for dc in docker-compose-5g-nsa-* ; do echo $dc ; DC_YAML="$DC_YAML -f $dc" ; done
 docker-compose $DC_YAML down --remove-orphans
 for network in cpn upn enb rfn ; do docker network rm $network ; done
+echo waiting for dovesnap to delete networks
+DSN=""
+while [ "$DSN" != "{}" ] ; do
+  DSN="$(wget -q -O- localhost:9401/networks)"
+  echo $DSN
+  sleep 1
+done
 cd dovesnap && docker-compose -f docker-compose.yml -f docker-compose-standalone.yml down && cd ..
 for volume in 5g_mongodb_data dovesnap_ovs-data ; do docker volume rm $volume ; done
