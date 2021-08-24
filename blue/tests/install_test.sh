b3bb8af94b486922d37d10964d0a9c3a918d9f52
Charlie Lewis <clewis@iqt.org>
2021-08-24 17:45:25 +0000

    forgot to add tests

---
+++ blue/tests/install_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive && \
+  echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+  sudo apt-get update && \
+  sudo apt-get remove docker docker-engine docker.io containerd runc && \
+  sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+  sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io python3-setuptools python3-dev && \
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+  sudo chmod +x /usr/local/bin/docker-compose && \
+  sudo pip3 install -r blue/requirements.txt && \
+  pushd blue && sudo python3 setup.py install && popd
