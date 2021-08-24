0969c3cfe3923a5b213df2eb826369d78c31377b
Charlie Lewis <clewis@iqt.org>
2021-08-24 18:27:48 +0000

    more quoting

--- blue/5G/open5gs/scripts/run_upf.sh
+++ blue/5G/open5gs/scripts/run_upf.sh
@@ -2,29 +2,28 @@
 
 set -e
 
-host=$(hostname)
 /etc/init.d/openvswitch-switch start
 
 i=0
-for ip in $* ; do
+for ip in "$*" ; do
 	i=$((i+1))
 	ifname=ogstap${i}
 	if [ "$i" -eq 1 ] ; then
 		ifname=ogstap
 	fi
-	echo ${ifname}: ${ip}
-	ip tuntap add name $ifname mode tap
-        br=${ifname}br
-	ovs-vsctl add-br $br
-	ovs-vsctl add-port $br $ifname
-	brmac=$(ovs-ofctl dump-ports-desc $br|grep LOCAL|grep -Eo '([a-f0-9:]{17,17})$')
-	ovs-ofctl del-flows $br
-	ovs-ofctl add-flow $br "in_port=$br,actions=output:$ifname"
-	ovs-ofctl add-flow $br "in_port=$ifname,,actions=set_field:$brmac->eth_dst,output:$br"
-	ip addr add $ip dev $br
-	ip link set $ifname up
-	ip link set $br up
-	iptables -I INPUT -i $br -j ACCEPT
+	echo "${ifname}": "${ip}"
+	ip tuntap add name "$ifname" mode tap
+        br="${ifname}"br
+	ovs-vsctl add-br "$br"
+	ovs-vsctl add-port "$br" "$ifname"
+	brmac=$(ovs-ofctl dump-ports-desc "$br"|grep LOCAL|grep -Eo '([a-f0-9:]{17,17})$')
+	ovs-ofctl del-flows "$br"
+	ovs-ofctl add-flow "$br" "in_port=$br,actions=output:$ifname"
+	ovs-ofctl add-flow "$br" "in_port=$ifname,,actions=set_field:$brmac->eth_dst,output:$br"
+	ip addr add "$ip" dev "$br"
+	ip link set "$ifname" up
+	ip link set "$br" up
+	iptables -I INPUT -i "$br" -j ACCEPT
 done
 
 ovs-vsctl show
