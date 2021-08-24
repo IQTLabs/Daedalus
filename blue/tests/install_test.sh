b8995f57be3c4c14e220db35d0d33341a99693f2
Charlie Lewis <clewis@iqt.org>
2021-08-24 17:47:41 +0000

    no pushd

--- blue/tests/install_test.sh
+++ blue/tests/install_test.sh
@@ -11,4 +11,4 @@ export DEBIAN_FRONTEND=noninteractive && \
   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
   sudo chmod +x /usr/local/bin/docker-compose && \
   sudo pip3 install -r blue/requirements.txt && \
-  pushd blue && sudo python3 setup.py install && popd
+  cd blue && sudo python3 setup.py install && cd ..
