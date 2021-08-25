e11fae50b7374648fbf5b0ba03e52c884e6d3c53
Charlie Lewis <clewis@iqt.org>
2021-08-25 16:42:25 +0000

    fix path

--- blue/tests/test_daedalus.py
+++ blue/tests/test_daedalus.py
@@ -17,8 +17,11 @@ def test_start_remove_dovesnap():
 
 def test_build_images():
     instance = Daedalus()
+    # hack conf_dir since it's not installed as a library
+    instance.set_config_dir(conf_dir='/..')
     instance.build_dockers(srsran=True, ueransim=True,
                            open5gs=True, srsran_lime=True)
+    instance.reset_cwd()
 
 
 def test_create_remove_networks():
