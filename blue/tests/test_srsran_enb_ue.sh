86037071f0535763de0791e2c8309d778a504550
Charlie Lewis <clewis@iqt.org>
2021-08-30 20:21:15 +0000

    fix paths

--- blue/tests/test_srsran_enb_ue.sh
+++ blue/tests/test_srsran_enb_ue.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "starting services..."
+cd blue/5G || exit 1
 SMF='' docker-compose -f core/epc.yml -f core/upn.yml -f core/db.yml -f SIMULATED/srsran-enb.yml -f SIMULATED/srsran-ue.yml up -d --build
 docker stop ue2
 docker stop ue3
@@ -10,3 +11,4 @@ echo "checking UE connectivity..."
 sleep 300
 docker logs ue
 docker exec ue ping -I tun_srsue -c1 google.com
+docker-compose -f core/epc.yml -f core/upn.yml -f core/db.yml -f SIMULATED/srsran-enb.yml -f SIMULATED/srsran-ue.yml down
--- blue/tests/test_ueransim_gnb_ue.sh
+++ blue/tests/test_ueransim_gnb_ue.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "starting services..."
+cd blue/5G || exit 1
 SMF='core' docker-compose -f core/epc.yml -f core/upn.yml -f core/db.yml -f core/core.yml -f SIMULATED/ueransim-gnb.yml -f SIMULATED/ueransim-ue.yml up -d --build
 
 echo "checking UE connectivity..."
 sleep 300
 docker logs ue1
 docker exec ue1 ping -I uesimtun0 -c1 google.com
+docker-compose -f core/epc.yml -f core/upn.yml -f core/db.yml -f core/core.yml -f SIMULATED/ueransim-gnb.yml -f SIMULATED/ueransim-ue.yml down
