d07e1e4e26006c7b3739b139eab1bf1a8f74c7c5
Ryan <rashley@iqt.org>
2022-07-07 08:41:07 -0400

    semi-functional

--- blue/setup.py
+++ blue/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='daedalus-5g',
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    setup_requires=['setuptools>=17.1'],
     python_requires='>=3.6',
     packages=['daedalus'],
     package_dir={'daedalus': '5G/daedalus'},
-    pbr=True
+    py_modules=['daedalus'],
 )
