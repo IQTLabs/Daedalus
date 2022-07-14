19f6e5341dd0188e5268ab5e25d44919a81cad09
Ryan <rashley@iqt.org>
2022-07-14 10:13:10 -0400

    debugging test issues

--- blue/tests/test_daedalus.py
+++ blue/tests/test_daedalus.py
@@ -9,7 +9,7 @@ from daedalus.daedalus import Daedalus
 def test_start_remove_dovesnap():
     instance = Daedalus()
     # hack conf_dir since it's not installed as a library
-    instance.set_config_dir(conf_dir='/..')
+    instance.set_config_dir(conf_dir='.')
     instance.start_dovesnap()
     instance.remove_dovesnap()
     instance.reset_cwd()
