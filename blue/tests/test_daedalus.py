bea7b32a063f453aaff8de68332957223c45c9ad
Ryan <rashley@iqt.org>
2022-07-15 14:21:51 -0400

    reset config copy

--- blue/tests/test_daedalus.py
+++ blue/tests/test_daedalus.py
@@ -15,8 +15,6 @@ def test_start_remove_dovesnap():
 
 def test_build_images():
     instance = Daedalus()
-    # hack conf_dir since it's not installed as a library
-    instance.set_config_dir(conf_dir='/Daedalus/blue/5G')
     instance.build_dockers(srsran=True, ueransim=True,
                            open5gs=True, srsran_lime=True)
     instance.reset_cwd()
@@ -24,8 +22,6 @@ def test_build_images():
 
 def test_create_remove_networks():
     instance = Daedalus()
-    # hack conf_dir since it's not installed as a library
-    instance.set_config_dir(conf_dir='/..')
     instance.start_dovesnap()
     instance.create_networks()
     instance.remove_networks()
