f10ceab80100c15ade0b5c661a87bf42f454cc53
Ryan <rashley@iqt.org>
2022-07-15 09:32:07 -0400

    basing the conf directory off of where tests seem to be running from 2

--- blue/tests/test_daedalus.py
+++ blue/tests/test_daedalus.py
@@ -18,7 +18,7 @@ def test_start_remove_dovesnap():
 def test_build_images():
     instance = Daedalus()
     # hack conf_dir since it's not installed as a library
-    instance.set_config_dir(conf_dir='/..')
+    instance.set_config_dir(conf_dir='/Daedalus/blue/5G')
     instance.build_dockers(srsran=True, ueransim=True,
                            open5gs=True, srsran_lime=True)
     instance.reset_cwd()
