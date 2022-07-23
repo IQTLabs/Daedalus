e9a627202d7946b7d076bbe781d3bc5d64794209
Ryan <rashley@iqt.org>
2022-07-23 09:35:56 -0400

    first shot at themeing

--- blue/5G/daedalus/styles.py
+++ blue/5G/daedalus/styles.py
@@ -1,9 +1,28 @@
-custom_style = { #get_style({
-    "separator": '#6C6C6C',
-    "questionmark": '#FF9D00 bold',
-    "selected": '#5F819D',
-    "pointer": '#FF9D00 bold',
-    #"instruction": '',  # default
-    "answer": '#5F819D bold',
-    #"question": '',
-} #, style_override=False)
+# custom_style = { #get_style({
+#     "separator": '#6C6C6C',
+#     "questionmark": '#FF9D00 bold',
+#     "selected": '#5F819D',
+#     "pointer": '#FF9D00 bold',
+#     #"instruction": '',  # default
+#     "answer": '#5F819D bold',
+#     #"question": '',
+# } #, style_override=False)
+
+custom_style = {
+    "Question": {
+        "mark_color": "orange bold",
+        "brackets_color": "orange bold",
+    },
+    "Checkbox": {
+        "selection_color": "lightskyblue3",
+        "selection_icon": "❯"
+        "selected_icon": "◉"
+        "selected_color": "lightskyblue3"
+        "unselected_color": "normal"
+        "unselected_icon": "◯"
+    },
+    "List"{
+        "selection_cursor": "❯"
+        "selection_color": "lightskyblue3"
+    }
+}
