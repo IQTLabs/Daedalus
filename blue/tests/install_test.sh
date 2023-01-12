98d3d0ec46057f4cd46a29a78e5dc69044250e76
Josh Bailey <josh@vandervecken.com>
2023-01-12 20:41:16 +0000

    specify iptables legacy.

--- blue/tests/install_test.sh
+++ blue/tests/install_test.sh
@@ -5,6 +5,7 @@ export DEBIAN_FRONTEND=noninteractive && \
   sudo apt-get update && \
   sudo apt-get remove docker docker-engine docker.io containerd runc && \
   sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+  sudo update-alternatives --set iptables /usr/sbin/iptables-legacy && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
   sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io python3-setuptools python3-dev uhd-host && \
