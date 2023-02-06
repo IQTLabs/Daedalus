d96e99d76757b499b0989fa73e346c7f38bb9a7c
Josh Bailey <josh@vandervecken.com>
2023-02-06 23:06:46 +0000

    ubuntu 22.04, flush ruleset.

--- blue/tests/install_test.sh
+++ blue/tests/install_test.sh
@@ -4,8 +4,9 @@ export DEBIAN_FRONTEND=noninteractive && \
   echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
   sudo apt-get update && \
   sudo apt-get remove docker docker-engine docker.io containerd runc && \
-  sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+  sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nftables && \
   sudo update-alternatives --set iptables /usr/sbin/iptables-legacy && \
+  sudo nft flush ruleset && \
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
   sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io python3-setuptools python3-dev uhd-host && \
