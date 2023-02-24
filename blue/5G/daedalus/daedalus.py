fd485327033dad22f95969947ca25bcb6e9a3fb1
Josh Bailey <josh@vandervecken.com>
2023-02-24 07:52:12 +0000

    1.1.7

--- blue/5G/daedalus/daedalus.py
+++ blue/5G/daedalus/daedalus.py
@@ -88,7 +88,7 @@ class Daedalus():
 
     def start_dovesnap(self):
         """Start Dovesnap components in Docker containers"""
-        release = 'v1.1.6'
+        release = 'v1.1.7'
         faucet_prefix = '/tmp/tpfaucet'
         sudo[ip['link', 'add', 'tpmirrorint', 'type', 'veth',
                 'peer', 'name', 'tpmirror']](retcode=(0, 2))
